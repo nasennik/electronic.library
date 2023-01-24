66f64e06a073912cd8e6b948203e5f8744fd0f59
Dora.stories <nasennik03@gmail.com>
2023-01-24 13:09:12 +0300

    fix database.properties for windows

--- src/main/java/com/electronic/library/config/SpringConfig.java
+++ src/main/java/com/electronic/library/config/SpringConfig.java
@@ -61,7 +61,7 @@ public class SpringConfig implements WebMvcConfigurer {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
         dataSource.setUrl(environment.getProperty("url"));
-        dataSource.setUsername(environment.getProperty("username"));
+        dataSource.setUsername(environment.getProperty("user_name"));
         dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
     }
